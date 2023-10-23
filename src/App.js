28695d7bdb988513136bcebba92f8c8a74203c88
Madalina <madalinaoancea@hotmail.com>
2023-10-23 17:42:10 -0400

    Update age and name

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react";
+import "./App.css";
+
+const initialValue = {
+  name: "Anne",
+  age: 25,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment": {
+      return {
+        ...state,
+        age: state.age + 1,
+      };
+    }
+
+    case "decrement": {
+      return {
+        ...state,
+        age: state.age - 1,
+      };
+    }
+
+    case "change_name": {
+      return {
+        ...state,
+        name: action.payload,
+      };
+    }
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialValue);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        value={state.name}
+        onChange={(event) =>
+          dispatch({ type: "change_name", payload: event.target.value })
+        }
+      />
+
+      <div>
+        <span>
+          {state.name}, {state.age} years old
+        </span>
+      </div>
+
+      <button onClick={() => dispatch({ type: "increment" })}>Increment</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
     </div>
   );
 }
